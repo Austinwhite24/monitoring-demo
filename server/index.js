87bf1258e9d5ab0bc55caee8b8fb7f747d2068cc
Austin White <basketballking19aw@gmail.com>
2022-02-24 12:11:11 -0700

    third commit rollbar

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const path = require('path')
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: 'd2b77c1914574a5d913bd4898cbcc55a',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 const app = express()
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
+
+// app.use(express.static(path.join(__dirname, '../')))
 
 const port = process.env.PORT || 4545
 
