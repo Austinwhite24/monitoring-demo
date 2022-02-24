e029125f0bd0f01abd722e54ffe1b60001f0ddf2
Austin White <basketballking19aw@gmail.com>
2022-02-24 12:40:01 -0700

    hope

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const path = require('path')
 
 // include and initialize the rollbar library with your access token
-var Rollbar = require("rollbar");
-var rollbar = new Rollbar({
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
   accessToken: 'd2b77c1914574a5d913bd4898cbcc55a',
   captureUncaught: true,
   captureUnhandledRejections: true
@@ -13,10 +13,27 @@ var rollbar = new Rollbar({
 rollbar.log("Hello world!");
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
+
+let students = []
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log('student was added succesfully', {author: 'Austin'})
+
+    res.status(200).send(students)
+})
+
+app.use(rollbar.errorHandler())
+
 
 // app.use(express.static(path.join(__dirname, '../')))
 
